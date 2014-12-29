c14601a0bc640e8176f2bc044572925e214818d6
Milos Milojevic <mmilojevic@telesign.com>
2014-12-29 17:13:16 +0100

    Check model fucntions usage in controller and model. check controller functions usage in views, js and controlllers

--- application/views/code_review.php
+++ application/views/code_review.php
@@ -19,21 +19,32 @@
             </thead>
             <tbody>
                 <?php foreach ($controllers as $controller): ?>
-                	
-                	<?php if (is_array($controller["filename"]) || $controller["filename"] === 'index.html') 
-			{
-				continue; // This is a directory not a file or an index.html file
-			}
-			?>
+
+                    <?php
+                    if (is_array($controller["filename"]) || $controller["filename"] === 'index.html') {
+                        continue; // This is a directory not a file or an index.html file
+                    }
+                    ?>
                     <tr>
                         <td><?php echo $controller["filename"]; ?></td>
-                        <td><?php echo byte_format($controller["size"]["size"]); ?></td>
-                        <td><?php echo date('d M Y', $controller["size"]["date"]); ?></td>
+                        <td><?php echo byte_format($controller["info"]["size"]); ?></td>
+                        <td><?php echo date('d M Y', $controller["info"]["date"]); ?></td>
                         <td>
                             <ol>
-                                <?php foreach ($controller["functions"] as $function): ?>
-                                    <li><?php echo $function; ?></li>
+
+                                 <?php foreach ($controller["functions_with_usage"] as $function => $usage_array): ?>
+                                    <li><?php echo $function; ?>()</li>
+                                    <ul>
+                                        <?php if (is_array($usage_array['usage']) && !empty($usage_array['usage'])): ?>
+                                            <?php foreach ($usage_array['usage'] as $uses): ?>
+                                                <li><?php echo $uses; ?></li>
+                                            <?php endforeach; ?>
+                                        <?php else: ?>
+                                            <li><span class="label label-warning">Appears to be unused</span></li>
+                                        <?php endif; ?>
+                                    </ul>
                                 <?php endforeach; ?>
+
                             </ol>
                         </td>
                     </tr>
@@ -56,30 +67,28 @@
             </thead>
             <tbody>
                 <?php foreach ($models as $model): ?>
-					
-			<?php if (is_array($model["filename"]) || $model["filename"] === 'index.html')
-			{
-				continue; // This is a directory not a file or an index.html file
-			}
-			?>
+
+                    <?php
+                    if (is_array($model["filename"]) || $model["filename"] === 'index.html') {
+                        continue; // This is a directory not a file or an index.html file
+                    }
+                    ?>
                     <tr>
                         <td><?php echo $model["filename"]; ?></td>
-                        <td><?php echo byte_format($model["size"]["size"]); ?></td>
-                        <td><?php echo date('d M Y', $model["size"]["date"]); ?></td>
+                        <td><?php echo byte_format($model["info"]["size"]); ?></td>
+                        <td><?php echo date('d M Y', $model["info"]["date"]); ?></td>
                         <td>
                             <ol>
                                 <?php foreach ($model["functions_with_usage"] as $function => $usage_array): ?>
                                     <li><?php echo $function; ?>()</li>
                                     <ul>
                                         <?php if (is_array($usage_array['usage']) && !empty($usage_array['usage'])): ?>
-
                                             <?php foreach ($usage_array['usage'] as $uses): ?>
                                                 <li><?php echo $uses; ?></li>
                                             <?php endforeach; ?>
-
                                         <?php else: ?>
                                             <li><span class="label label-warning">Appears to be unused</span></li>
-                                        <?php endif; ?>
+                                            <?php endif; ?>
                                     </ul>
                                 <?php endforeach; ?>
                             </ol>
